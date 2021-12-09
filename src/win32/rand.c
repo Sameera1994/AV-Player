a077251ad1c32726947afcdc00573afb6b73b2bf
Sameera Lakshitha <lakshithasameera89@gmail.com>
2021-12-10 01:00:38 +0530

    Create rand.c

---
+++ src/win32/rand.c
@@ -0,0 +1,24 @@
+/*****************************************************************************
+ * rand.c : non-predictible random bytes generator
+ *****************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <vlc_common.h>
+#include <vlc_rand.h>
+
+#include <bcrypt.h>
+
+void vlc_rand_bytes (void *buf, size_t len)
+{
+    BCRYPT_ALG_HANDLE algo_handle;
+    NTSTATUS ret = BCryptOpenAlgorithmProvider(&algo_handle, BCRYPT_RNG_ALGORITHM,
+                                               MS_PRIMITIVE_PROVIDER, 0);
+    if (BCRYPT_SUCCESS(ret))
+    {
+        BCryptGenRandom(algo_handle, buf, len, 0);
+        BCryptCloseAlgorithmProvider(algo_handle, 0);
+    }
+}
